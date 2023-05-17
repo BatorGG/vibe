0a2a43c09cad0e70c388c92956151e7da67e87fd
BatorGG <bator.karpathegyi@gmail.com>
2023-05-17 18:51:31 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
 var button = document.getElementById("vibe");
+var play = document.getElementById("play");
+var downTime = 0;
+var upTime = 0;
+var pattern = [];
 
 button.addEventListener("touchstart", () => {
-    navigator.vibrate(1000);
+    navigator.vibrate(2000);
+    downTime = new Date().getTime();
+
+    var timeDifference = downTime - upTime;
+    if (downTime == 0 || upTime == 0) {
+        timeDifference = 0;
+    }
+    else {
+        pattern.push(timeDifference);
+    }
     console.log("Start")
+    console.log(`Waited for ${timeDifference}ms`);
 })
 
 button.addEventListener("touchend", () => {
     navigator.vibrate(0);
+
+    upTime = new Date().getTime();
+    var timeDifference = upTime - downTime;
+    pattern.push(timeDifference);
     console.log("end");
+    console.log(`Button held down for ${timeDifference}ms`);
+    console.log(pattern);
+})
+
+play.addEventListener("click", () => {
+    navigator.vibrate(pattern);
+    console.log("Playing");
 })
